8f91b87622c58eb436641705086b4edbd14ecad2
nagasuhas06 <nagasuhas06@gmail.com>
2024-11-21 11:34:00 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,5 @@
 mvn clean package 
-echo "suhas" | sudo -S your_command
-cd target
+echo "suhas" | sudo -S cd target
 sudo cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
 cd /home/ubuntu/apache-tomcat-9.0.97/bin/
 ./shutdown.sh
