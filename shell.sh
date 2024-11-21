04458ad81926f69bddd2a1eb49c3bd94b809cc6c
nagasuhas06 <nagasuhas06@gmail.com>
2024-11-21 11:28:39 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,7 +1,9 @@
+sudo visudo
+ubuntu ALL=(ALL) NOPASSWD: ALL
 sudo su
 mvn clean package 
 cd target
-sudo cp -r area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
+sudo cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
 cd /home/ubuntu/apache-tomcat-9.0.97/bin/
 ./shutdown.sh
 ./startup.sh
