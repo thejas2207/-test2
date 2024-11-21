31378dd38081a65959a8ce4c233dec970be0f39b
nagasuhas06 <nagasuhas06@gmail.com>
2024-11-21 11:37:25 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,6 @@
 mvn clean package 
-echo "suhas" | sudo -S cd target
+sudo visudo
+jenkins ALL=(ALL) NOPASSWD: ALL
 sudo cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
 cd /home/ubuntu/apache-tomcat-9.0.97/bin/
 ./shutdown.sh
