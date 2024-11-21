ea6be74eddfe4e0c7b7bb634c9353f75488c4776
nagasuhas06 <nagasuhas06@gmail.com>
2024-11-21 11:49:44 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,3 +3,4 @@ cd target
 sudo cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
 sudo bash /home/ubuntu/apache-tomcat-9.0.97/bin/shutdown.sh
 sudo bash /home/ubuntu/apache-tomcat-9.0.97/bin/startup.sh
+echo "Successfully deployed"
