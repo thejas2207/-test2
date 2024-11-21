eabad22b4955086fb84bddc0b0defc1d394568c7
nagasuhas06 <nagasuhas06@gmail.com>
2024-11-21 11:41:33 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 mvn clean package 
+cd target
 sudo cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
-cd /home/ubuntu/apache-tomcat-9.0.97/bin/
-./shutdown.sh
-./startup.sh
+bash /home/ubuntu/apache-tomcat-9.0.97/bin/shutdown.sh
+bash /home/ubuntu/apache-tomcat-9.0.97/bin/startup.sh
