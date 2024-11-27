8481267809a3a48bf62d79666b73e454c5fdd6a5
nagasuhas06 <nagasuhas06@gmail.com>
2024-11-27 13:26:07 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 mvn clean package
 cd target
-cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
+sudo cp area-calculator-1.0-SNAPSHOT.war /home/ubuntu/apache-tomcat-9.0.97/webapps
 cd /home/ubuntu/apache-tomcat-9.0.97/bin
 ./shutdwon.sh
 ./startup.sh
